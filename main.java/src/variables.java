7fd00af0ba82b53f4133eca08bdc650e1df0ca88
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2026-07-03 23:24:10 +0100

    Create variables.java

---
+++ main.java/src/variables.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        String us = "United States";
+        int population = 331002651;
+        
+        // Don't change below this line
+        System.out.println("us = " + us);
+        System.out.println("population = " + population);
+    }
+}
